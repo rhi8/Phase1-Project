45e03b013679f6193ef36b497bd1001279d39934
rhitesh <adarshnuckched1@gmail.com>
2020-12-13 04:10:42 +0400

    fialProject

--- function.js
+++ function.js
@@ -1,46 +1,27 @@
+//connect all function in 1
 function connectAll() {
-    var formData = readForm()
-    display(formData)
-    //onClick()
+    var formData = readForm();
+    display(formData);
+    resetForm()
+   
     //removeElement()
 }
+
+//read input from html
 function readForm() {
 var formData = document.getElementById("textarea").value;
-return  formData
+        return  formData;
 }
+//display input in new div
 function display(data) {
-let newDiv = document.createElement('div');
-newDiv.innerHTML=data;
-var list = document.getElementById("test");
-list.insertBefore(newDiv, list.childNodes[0])
+let newDiv = document.createElement('div');//create new div
+newDiv.innerHTML=data; //change div to input value
+var list = document.getElementById("test");//select div need to manupulate
+list.insertBefore(newDiv, list.childNodes[0]);//lis blog newest to the top
 }
 
-
-/*function onClick(){
-let newSpan = document.createElement('div');
-newSpan.id="delco";
-newSpan.innerHTML="deleteeeeeeeeeeeeeeee";
-document.body.appendChild(newSpan)
-var list = document.getElementById("delco");
-list.insertBefore(newSpan, list.childNodes[0]);
-}
-
-
-
-
-
-
-
-
-/*function display(data) {
-    let newDiv = document.createElement('div');
-    newDiv.id="test";
-    newDiv.innerHTML=data;
-    document.body.appendChild(newDiv)
-    var list = document.getElementById("test");
-    list.insertBefore(newDiv, list.childNodes[0]);
+//resetdata
+function resetForm() {
+    document.getElementById("textarea").value="";
     
-    }*/
-
-
-
+}
